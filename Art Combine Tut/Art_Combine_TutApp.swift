283dcbe55a33f283d560a97fa85d2496861d480c
Tlanetzi Chavez <chaveztlanetzi14@gmail.com>
2024-03-12 17:32:49 +0100

    Initial Commit

---
+++ Art Combine Tut/Art_Combine_TutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_Combine_TutApp.swift
+//  Art Combine Tut
+//
+//  Created by Tlanetzi Chavez Madero on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Art_Combine_TutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
